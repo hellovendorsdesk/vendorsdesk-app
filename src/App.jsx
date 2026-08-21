81eb75e288f310c8ea7f3b9d77583f587eef1053
hellovendorsdesk <hellovendorsdesk@gmail.com>
2026-08-21 22:43:36 +0530

    Revert App.jsx on main branch to clean state

--- src/App.jsx
+++ src/App.jsx
@@ -1878,7 +1878,8 @@ export default function App() {
                 top: 0,
                 bottom: 0,
                 left: 0,
-                zIndex: 100
+                zIndex: 100,
+                overflowY: 'auto'
             }}>
                 <div>
                     {/* Brand */}
@@ -1977,7 +1978,7 @@ export default function App() {
             </aside>
 
             {/* Main Area */}
-            <div style={{ marginLeft: '260px', flexGrow: 1, padding: '2.5rem', display: 'flex', justifyContent: 'center' }}>
+            <div style={{ marginLeft: '260px', width: 'calc(100% - 260px)', flexGrow: 1, padding: '2.5rem', display: 'flex', justifyContent: 'center' }}>
                 <div style={{ width: '100%', maxWidth: '100%' }}>
                     
                     {/* Top 0-Credits Banner Notice */}
